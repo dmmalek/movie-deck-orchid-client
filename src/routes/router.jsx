1843be0cebda61929cad2f2d97e5311bb92250ed
Abdul Malek <abdulmalekmonir97@gmail.com>
2025-09-30 22:26:27 +0600

    set up auth_context

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,6 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
 import Home from "../Home/Home";
+import AuthLayout from "../layouts/AuthLayout";
+import LogIn from "../auth/LogIn";
+import Register from "../auth/Register";
 
 const router = createBrowserRouter([
   {
@@ -10,6 +13,20 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <Home />,
+      },
+    ],
+  },
+  {
+    path: "/auth",
+    element: <AuthLayout />,
+    children: [
+      {
+        path: "/auth/login",
+        element: <LogIn />,
+      },
+      {
+        path: "/auth/register",
+        element: <Register />,
       },
     ],
   },
