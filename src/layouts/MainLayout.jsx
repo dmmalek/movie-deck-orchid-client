fba5c2f13a8fe4c411dbd4f730dd5979471772ac
Abdul Malek <abdulmalekmonir97@gmail.com>
2025-09-30 20:03:32 +0600

    add layout ad route

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom";
+import Footer from "../component/Footer";
+import Navbar from "../component/Navbar";
+
+const MainLayout = () => {
+  return (
+    <div>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
+
+export default MainLayout;
